7ac42ad9ae857d2590d4533560a51f0386e181a1
David Elliott <delliott@caltech.edu>
2017-08-15 19:44:11 -0700

    Add basic templates and views for testing

--- avery_website/views.py
+++ avery_website/views.py
@@ -3,5 +3,21 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@main.route('/gallery')
+def gallery():
+    return 'This is the gallery'
+
+@main.route('/calendar')
+def calendar():
+    return 'This is the calendar'
+
+@main.route('/government')
+def government():
+    return 'This is the government page'
+
+@main.route('/tools')
+def tools():
+    return 'This is the tools page'
