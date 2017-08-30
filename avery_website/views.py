6cca8bcecfe4fc92fa036a53c957f9536eddd547
David Elliott <delliott@caltech.edu>
2017-08-30 14:47:11 -0600

    Get album cover photo display working

--- avery_website/views.py
+++ avery_website/views.py
@@ -1,4 +1,11 @@
+import facebook
 from flask import Blueprint, render_template
+
+FACEBOOK_APP_ID = '261003164413097'
+FACEBOOK_APP_SECRET = '31c63be403ce8ac70e6d1441ebe84b85'
+IMAGE_SIZE_INDEX = 2
+
+graph = facebook.GraphAPI(FACEBOOK_APP_ID + '|' + FACEBOOK_APP_SECRET)
 
 main = Blueprint('main', __name__)
 
@@ -8,7 +15,15 @@ def index():
 
 @main.route('/gallery')
 def gallery():
-    return render_template('gallery.html')
+    albums = graph.get_object(id='averyglory/albums', fields='name,cover_photo{images}')
+    for album in albums['data']:
+        album['cover_photo']['medium_index'] = min(IMAGE_SIZE_INDEX, len(album['cover_photo']['images']) - 1)
+    print(albums)
+    return render_template('gallery.html', albums=albums)
+
+@main.route('/gallery/album/<album_id>')
+def album(album_id):
+    return 'Album ' + album_id
 
 @main.route('/calendar')
 def calendar():
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+#!./venv/bin/python3
+
 from avery_website import create_app
 
 app = create_app()
