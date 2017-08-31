6e0ccb960eafb8785eb82af06cd82913edcf1ff8
David Elliott <delliott@caltech.edu>
2017-08-31 15:48:02 -0600

    Add sticky navbar

--- avery_website/views.py
+++ avery_website/views.py
@@ -1,9 +1,10 @@
 import facebook
+import re
 from flask import Blueprint, render_template
 
 FACEBOOK_APP_ID = '261003164413097'
 FACEBOOK_APP_SECRET = '31c63be403ce8ac70e6d1441ebe84b85'
-IMAGE_SIZE_INDEX = 2
+IMAGE_SIZE_INDEX = 5
 
 graph = facebook.GraphAPI(FACEBOOK_APP_ID + '|' + FACEBOOK_APP_SECRET)
 
@@ -18,6 +19,7 @@ def gallery():
     albums = graph.get_object(id='averyglory/albums', fields='name,cover_photo{images}')
     for album in albums['data']:
         album['cover_photo']['medium_index'] = min(IMAGE_SIZE_INDEX, len(album['cover_photo']['images']) - 1)
+        album['name_stripped'] = re.sub("\[([^]]+)\]", "", album['name'])
     print(albums)
     return render_template('gallery.html', albums=[album for album in albums['data'] if album['name'] != 'Profile Pictures' and album['name'] != 'Cover Photos'])
 
