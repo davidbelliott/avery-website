edac4d1d8fbfa843825208fa18b35d1bbab62cb0
David Elliott <delliott@caltech.edu>
2017-09-01 11:31:11 -0600

    Google Calendar event importing; sensitive data moved to instance dir

--- avery_website/__init__.py
+++ avery_website/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object('config')
+    app.config.from_pyfile('config.py')
 
     from .views import main as main
     app.register_blueprint(main)
--- avery_website/views.py
+++ avery_website/views.py
@@ -1,14 +1,42 @@
 import facebook
 import re
-from flask import Blueprint, render_template
+import httplib2
+import os
 
-FACEBOOK_APP_ID = '261003164413097'
-FACEBOOK_APP_SECRET = '31c63be403ce8ac70e6d1441ebe84b85'
-IMAGE_SIZE_INDEX = 5
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools as tools
+from oauth2client.file import Storage
 
-graph = facebook.GraphAPI(FACEBOOK_APP_ID + '|' + FACEBOOK_APP_SECRET)
+import datetime
+
+from flask import Blueprint, render_template, current_app
 
 main = Blueprint('main', __name__)
+
+def google_get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_dir = 'instance/credentials'
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir, 'avery_website.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(current_app.config['GOOGLE_CLIENT_SECRET_FILE'], current_app.config['GOOGLE_SCOPES'])
+        flow.user_agent = current_app.config['GOOGLE_APPLICATION_NAME']
+        flow.params['access_type'] = 'offline'
+        credentials = tools.run_flow(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
 
 @main.route('/')
 def index():
@@ -16,30 +44,45 @@ def index():
 
 @main.route('/gallery')
 def gallery():
+    graph = facebook.GraphAPI(current_app.config['FACEBOOK_APP_ID'] + '|' + current_app.config['FACEBOOK_APP_SECRET'])
     albums = graph.get_object(id='averyglory/albums', fields='name,cover_photo{images}')
     for album in albums['data']:
-        album['cover_photo']['medium_index'] = min(IMAGE_SIZE_INDEX, len(album['cover_photo']['images']) - 1)
+        album['cover_photo']['medium_index'] = min(current_app.config['FACEBOOK_IMAGE_SIZE_INDEX'], len(album['cover_photo']['images']) - 1)
         album['name_stripped'] = re.sub("\[([^]]+)\]", "", album['name'])
     print(albums)
     return render_template('gallery.html', albums=[album for album in albums['data'] if album['name'] != 'Profile Pictures' and album['name'] != 'Cover Photos'])
 
 @main.route('/gallery/album/<album_id>')
 def gallery_album(album_id):
+    graph = facebook.GraphAPI(current_app.config['FACEBOOK_APP_ID'] + '|' + current_app.config['FACEBOOK_APP_SECRET'])
     album = graph.get_object(id=album_id, fields='name, photos{images}')
     print(album)
     for photo in album['photos']['data']:
-        photo['medium_index'] = min(IMAGE_SIZE_INDEX, len(photo['images']) - 1)
+        photo['medium_index'] = min(current_app.config['FACEBOOK_IMAGE_SIZE_INDEX'], len(photo['images']) - 1)
     print(album)
     return render_template('album.html', album=album)
 
 @main.route('/calendar')
 def calendar():
-    return render_template('calendar.html')
+    credentials = google_get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+    return render_template('calendar.html', events=events)
 
 @main.route('/government')
 def government():
     return render_template('government.html')
-
-@main.route('/tools')
-def tools():
-    return 'This is the tools page'
