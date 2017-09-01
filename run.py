5a7d1ed46dd8ab65bc8aa7494cca32d7f19f76f4
David Elliott <delliott@caltech.edu>
2017-08-31 18:44:04 -0600

    Modify run.py

--- run.py
+++ run.py
@@ -2,5 +2,8 @@
 
 from avery_website import create_app
 
-app = create_app()
-app.run()
+if __name__ == '__main__'
+    app = create_app()
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
