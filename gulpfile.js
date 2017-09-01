72c2e1429d6da13d3184be01c9a7f216b8985e6d
David Elliott <delliott@caltech.edu>
2017-09-01 10:15:52 -0600

    More aesthetic updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('sass', function() {
     return gulp.src('./avery_website/assets/sass/*.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./avery_website/static/css'));
 });
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./avery_website/assets/sass/*.sass', ['sass']);
+});
+
+gulp.task('default', ['sass:watch']);
