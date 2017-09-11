bcc9e1bd2becd9ae66b92e03bed2dc98426cfd31
David Elliott <delliott@caltech.edu>
2017-09-11 16:01:37 +0000

    Minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
     return gulp.src('./avery_website/assets/sass/*.sass')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./avery_website/static/css'));
 });
 
