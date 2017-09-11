e433ac0850e067e54cb24879b8757051a6b3a815
David Elliott <delliott@caltech.edu>
2017-09-11 10:13:06 -0600

    Add CSS minification and uncss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uncss = require('gulp-uncss');
 
 gulp.task('sass', function() {
     return gulp.src('./avery_website/assets/sass/*.sass')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(uncss({
+            html: ['./avery_website/templates/*.html']
+        }))
         .pipe(gulp.dest('./avery_website/static/css'));
 });
 
